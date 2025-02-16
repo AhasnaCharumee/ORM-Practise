13e012aa82e0c25619908bfb761190cf871c0597
AhasnaCharumee <charumeeahasna@gmail.com>
2025-02-17 03:45:26 +0530

    get customer where salary name address

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,23 +5,28 @@ import org.hibernate.Transaction;
 
 public class Main {
     public static void main(String[] args) {
-    Customer c1 = new Customer();
-    c1.setId("C001");
-    c1.setName("Lithira");
-    c1.setSalary(24000.00);
-    c1.setAddress("Palalla");
+//    Customer c1 = new Customer();
+//    c1.setId("C001");
+//    c1.setName("Lithira");
+//    c1.setSalary(24000.00);
+//    c1.setAddress("Palalla");
 
     Customer c2 = new Customer();
-         c2.setId("C002");
-         c2.setName("Lihini");
-         c2.setSalary(50000.00);
-         c2.setAddress("Matara");
+//         c2.setId("C002");
+//         c2.setName("Lihini");
+//         c2.setSalary(50000.00);
+//         c2.setAddress("Matara");
 
     Session session = FactoryConfiguration.getInstance().getSession();
     Transaction transaction = session.beginTransaction();
 //    session.save(c2);
-        session.update(c2);
-    transaction.commit();
+//        session.update(c2);
+
+        Customer c1 = session.get(Customer.class,"C001");
+//    System.out.println(c1);
+//        System.out.println(c1.getAddress());
+        System.out.println(c1.getName() + "-" + c1.getSalary());
+        transaction.commit();
     session.close();
     }
 }
