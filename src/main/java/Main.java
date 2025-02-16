639350b4b1321330eb21648c383ad1ee42b74280
AhasnaCharumee <charumeeahasna@gmail.com>
2025-02-17 03:37:58 +0530

    update attribute values

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,17 +9,18 @@ public class Main {
     c1.setId("C001");
     c1.setName("Lithira");
     c1.setSalary(24000.00);
-    c1.setAddress("Galle");
+    c1.setAddress("Palalla");
 
     Customer c2 = new Customer();
          c2.setId("C002");
          c2.setName("Lihini");
-         c2.setSalary(25000.00);
+         c2.setSalary(50000.00);
          c2.setAddress("Matara");
 
     Session session = FactoryConfiguration.getInstance().getSession();
     Transaction transaction = session.beginTransaction();
-    session.save(c2);
+//    session.save(c2);
+        session.update(c2);
     transaction.commit();
     session.close();
     }
